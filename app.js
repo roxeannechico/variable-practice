a7b8ffa4f11faacc40607b0b9dd32f41627e6833
roxeannechico <roseannechico22@gmail.com>
2024-09-10 08:45:13 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let name = 'Rose Anne Chico';
+console.log(name);
+
+let whatDoYouWannaBecomeInYourLife = 'Programmer';
+console.log(whatDoYouWannaBecomeInYourLife);
+
+let gender = 'Girl';
+console.log(gender);
+
